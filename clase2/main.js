dc043ad2810cfb15aed0d72ea8cbabb5f11732a9
Serj <sergiorojas.815@gmail.com>
2023-08-09 22:02:56 -0300

    first commit

---
+++ clase2/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let nombre = "Hola mundo";
+let edad = 40;
+const numeros = [1, 2, 3];
+console.log(nombre);
---
+++ clase2/main.ts
@@ -0,0 +1,7 @@
+let nombre : string = "Hola mundo"
+
+let edad = 40;
+
+const numeros : number[] = [1,2,3]
+
+console.log(nombre)
